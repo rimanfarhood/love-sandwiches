8df7fa9e86b4cce29bc3db017ab8d96bd7e2cb2e
Riman Farhood <riman.farhood@gmail.com>
2023-09-19 15:59:17 +0000

    Add requirements for deployment.

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -23,7 +22,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
 
         sales_data = data_str.split(",")
          
@@ -54,26 +53,6 @@ def validate_data(values):
     return True
 
 
-# def update_sales_worksheet(data):
-#     """
-#     Update sales worksheet, add new row with the list data provided.
-#     """
-#     print('Updating sales worksheet...\n')
-#     sales_worksheet = SHEET.worksheet('sales')
-#     sales_worksheet.append_row(data)
-#     print('Sales worksheet updated succsessfully.\n')
-
-# def update_surplus_worksheet(data):
-#     """
-#     Update surplus worksheet, add new row with the list data provided
-#     """
-#     print('Updating surplus worksheet...\n')
-#     surplus_worksheet = SHEET.worksheet('surplus')
-#     surplus_worksheet.append_row(data)
-#     print('Surplus worksheet updated successfully.\n')
-
-
-
 def update_worksheet(data, worksheet):
     """
     Receives a list of integers to be inserted into a worksheet
@@ -83,7 +62,6 @@ def update_worksheet(data, worksheet):
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully.\n')
-
 
 
 def calculate_surplus_data(sales_row):
@@ -105,6 +83,7 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+
 def get_last_5_enteries_sales():
     """
     Collects columns of data from sales worksheet, collecting
@@ -119,6 +98,7 @@ def get_last_5_enteries_sales():
         columns.append(column[-5:])
     
     return columns
+
 
 def calculate_stock_data(data):
     """
@@ -136,7 +116,6 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
-
 def main():
     """
     Run all program functions
@@ -149,6 +128,7 @@ def main():
     sales_columns = get_last_5_enteries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+
 
 print('Welcome to Love Sandwiches Data Automation')
 main()
